fe15935c528554efe44460c9af60814fafc9d20d
PASQUALE MAINOLFI <pm@MacBook-Pro-PM.local>
2023-06-22 16:43:42 +0200

    add zeropad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+mod libs {
+    
+    pub mod fft {
+        pub mod transform;
+    }
+    pub mod mp {}
+    pub mod operations {}
+    pub mod segmentation {}
+}
+
+use libs::fft::transform;
+
 fn main() {
-    println!("Hello, world!");
+
+    let x: Vec<f64> = (0..2000).map(|x| x as f64).collect();
+    let z = transform::zeropad(&x);
+    println!("{}\n", z.len());
+    println!("{z:?}\n");
+    
+    
 }
